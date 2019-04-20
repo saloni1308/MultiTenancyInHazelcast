02c505b20b0611449151955df957432ade796e20
saloni1308 <saloni.gupta1308@gmail.com>
2019-04-20 13:43:22 +0530

    Update CustomerController.java

--- src/main/java/com/architecture/prod/controller/CustomerController.java
+++ src/main/java/com/architecture/prod/controller/CustomerController.java
@@ -1,16 +1,3 @@
-/*
- * This material is the confidential, unpublished property
- * of Fair Isaac Corporation.  Receipt or possession
- * of this material does not convey rights to divulge,
- * reproduce, use, or allow others to use it without
- * the specific written authorization of Fair Isaac
- * Corporation and use must conform strictly to the
- * license agreement.
- *
- * Copyright (c) Fair Isaac Corporation, 2016
- * All Rights Reserved.
- */
-
 package com.architecture.prod.controller;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
